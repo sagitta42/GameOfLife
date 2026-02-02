3ea4b4f8c8d58daba18a3c62f71a38bdcf2e239a
sagitta42 <mariia.redchuk@gmail.com>
2026-02-02 20:39:21 +0100

    project version display header with version at game start

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Reflection;
+
 namespace GameOfLife
 {
     internal class Program
@@ -12,6 +14,8 @@ namespace GameOfLife
         static void Main(string[] args)
         {
             ConsoleManager console_manager = new ConsoleManager();
+            ShowHeader(console_manager);
+
             InputManager input_manager = new InputManager(console_manager);
 
             bool keep_playing = true;
@@ -50,6 +54,30 @@ namespace GameOfLife
                 }
 
             }
+        }
+
+        private static void ShowHeader(ConsoleManager console_manager)
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
+            string v = $"v{version.Major}.{version.Minor}.{version.MinorRevision}";
+            string header = "|o GameOfLife " + v + " o|";
+
+            string frame = new string('-', header.Length - 2);
+            frame = " " + frame + " ";
+            string subframe = "";
+            string cell;
+            for (int i = 0; i < header.Length - 2; i++)
+            {
+                cell = i % 2 == 0 ? " " : "o";
+                subframe = subframe + cell;
+            }
+            subframe = "|" + subframe + "|";
+
+            console_manager.WriteLine(frame);
+            console_manager.WriteLine(subframe);
+            console_manager.WriteLine(header);
+            console_manager.WriteLine(subframe);
+            console_manager.WriteLine(frame);
         }
     }
 }
