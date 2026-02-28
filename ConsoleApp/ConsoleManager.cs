0f2372942026deaacc557af03668dc5e6084f629
sagitta42 <mariia.redchuk@gmail.com>
2026-02-28 14:19:49 +0100

    separate GameAdapter to recycle text representation #17

--- ConsoleApp/ConsoleManager.cs
+++ ConsoleApp/ConsoleManager.cs
@@ -5,12 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 using GameLogic;
+using GameAdapter;
 
 namespace ConsoleApp
 {
     public class ConsoleManager
     {
-        private int n_world_rows;
         private int n_lines_skip = 0;
 
         public void WriteLine(string message = "", int sleep_time = 0)
@@ -32,9 +32,11 @@ namespace ConsoleApp
         {
             if (overwrite)
             {
+                int n_world_rows;
+                n_world_rows = world.height + 2;
                 Console.SetCursorPosition(0, Console.CursorTop - n_world_rows - n_lines_skip);
             }
-            string[] rows = GetWorldRows(world);
+            string[] rows = TextAdapter.GetWorldRows(world);
             foreach (string row in rows)
             {
                 Console.WriteLine(row);
@@ -48,35 +50,6 @@ namespace ConsoleApp
                 n_lines_skip = 0;
             }
             System.Threading.Thread.Sleep(sleep_time);
-        }
-
-        private string[] GetWorldRows(World world)
-        {
-            this.n_world_rows = world.height + 2;
-
-            string[] arr = new string[n_world_rows];
-            string frame = new string('-', world.length);
-            frame = ' ' + frame + ' ';
-            arr[0] = frame;
-            arr[n_world_rows - 1] = frame;
-
-            for (int j = 1; j < n_world_rows - 1; j++)
-            {
-                string row = "";
-                for (int i = 0; i < world.length; i++)
-                {
-                    row += CellRepr(world.GetCell(i, j - 1));
-                }
-                row = '|' + row + '|';
-                arr[j] = row;
-            }
-
-            return arr;
-        }
-        private string CellRepr(Cell cell)
-        {
-            string ret = cell.IsAlive() ? "o" : " ";
-            return ret;
         }
     }
 }
---
+++ GameAdapter/TextAdapter.cs
@@ -0,0 +1,37 @@
+﻿using GameLogic;
+
+namespace GameAdapter
+{
+    public static class TextAdapter
+    {
+        public static string[] GetWorldRows(World world)
+        {
+            int n_world_rows;
+            n_world_rows = world.height + 2;
+
+            string[] arr = new string[n_world_rows];
+            string frame = new string('-', world.length);
+            frame = ' ' + frame + ' ';
+            arr[0] = frame;
+            arr[n_world_rows - 1] = frame;
+
+            for (int j = 1; j < n_world_rows - 1; j++)
+            {
+                string row = "";
+                for (int i = 0; i < world.length; i++)
+                {
+                    row += CellRepr(world.GetCell(i, j - 1));
+                }
+                row = '|' + row + '|';
+                arr[j] = row;
+            }
+
+            return arr;
+        }
+        private static string CellRepr(Cell cell)
+        {
+            string ret = cell.IsAlive() ? "o" : " ";
+            return ret;
+        }
+    }
+}
