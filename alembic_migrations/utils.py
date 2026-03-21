8aedf39a2f66cef3da2ae9e216ae053d4958bb02
sagitta42 <mariia.redchuk@gmail.com>
2026-03-21 13:09:56 +0100

    pattern template table

---
+++ alembic_migrations/utils.py
@@ -0,0 +1,40 @@
+import enum
+from typing import Union
+
+from alembic import op
+from pydantic import BaseModel
+import sqlalchemy as sa
+
+class ColumnType(str, enum.Enum):
+    int = "int"
+    str = "str"
+
+class SaColumnType(enum.Enum):
+    int = sa.Integer
+    str = sa.String
+
+class Column(BaseModel):
+    name: str
+    description: str
+    type: ColumnType
+    primary_key: bool = False
+    nullable: bool = False
+
+    @property
+    def sa_type(self) -> sa.types.TypeEngine:
+        return SaColumnType[self.type].value()
+
+class Table(BaseModel):
+    name: str
+    columns: list[Column]
+
+def add_column_descriptions(table: Table):
+    for column in table.columns:
+        op.execute(f"""
+        EXEC sp_addextendedproperty 
+            @name = N'MS_Description',
+            @value = N'{column.description}',
+            @level0type = N'SCHEMA', @level0name = 'dbo',
+            @level1type = N'TABLE',  @level1name = '{table.name}',
+            @level2type = N'COLUMN', @level2name = '{column.name}';
+        """)
---
+++ alembic_migrations/versions/2026_03_21_1128-74d9f1e6e64f_pattern_templates_table.py
@@ -0,0 +1,42 @@
+"""pattern templates table
+
+Revision ID: 74d9f1e6e64f
+Revises: 
+Create Date: 2026-03-21 11:28:41.037080
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+from alembic_migrations import utils
+
+
+# revision identifiers, used by Alembic.
+revision: str = '74d9f1e6e64f'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+table_patterns = utils.Table(
+    name="pattern_templates",
+    columns=[
+        utils.Column(name="id", description="ID", type="int", primary_key=True),
+        utils.Column(name="name", description="Pattern name", type="str"),
+        utils.Column(name="type", description="Pattern type", type="str"),
+    ]
+)
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        table_patterns.name,
+        *(sa.Column(c.name, c.sa_type, nullable=c.nullable, primary_key=c.primary_key) for c in table_patterns.columns)
+    )
+    utils.add_column_descriptions(table_patterns)
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table(table_patterns.name)
