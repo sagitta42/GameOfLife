63771b160f2c9e53b632d0e4e2abd5a4ef41250f
sagitta42 <mariia.redchuk@gmail.com>
2026-03-24 23:00:52 +0100

    fix: toad pattern type oscillator

--- alembic_migrations/versions/2026_03_21_1838-c7179016fe7a_add_pattern_toad.py
+++ alembic_migrations/versions/2026_03_21_1838-c7179016fe7a_add_pattern_toad.py
@@ -5,6 +5,7 @@ Revises: 1bf804167a7e
 Create Date: 2026-03-21 18:38:44.569296
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -14,13 +15,14 @@ from alembic_migrations import models, utils
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'c7179016fe7a'
-down_revision: Union[str, Sequence[str], None] = '1bf804167a7e'
+revision: str = "c7179016fe7a"
+down_revision: Union[str, Sequence[str], None] = "1bf804167a7e"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
-pattern = models.Pattern(id=2, name="toad", type="stillife")
-coordinates = [(0,1), (0,2), (1,3), (2,0), (3,1), (3,2)]
+pattern = models.Pattern(id=2, name="toad", type="oscillator")
+coordinates = [(0, 1), (0, 2), (1, 3), (2, 0), (3, 1), (3, 2)]
+
 
 def upgrade() -> None:
     """Upgrade schema."""
